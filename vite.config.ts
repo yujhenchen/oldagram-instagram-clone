0ae985c9f8e2248ea5d90fc643b57100841ab464
yujhenchen <yujhen.chen93@gmail.com>
2023-11-30 10:32:10 +0800

    use unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS(),],
 })
